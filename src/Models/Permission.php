23840c90948f8f7dec0f7b48a44cf96200503d44
Ata <quetzalskeith@gmail.com>
2023-07-25 08:28:25 +0700

    Changed config path

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -8,7 +8,7 @@ class Permission extends SpatiePermission
 {
     public function __construct(array $attributes = [])
     {
-        $this->connection = config('client.database.connection');
+        $this->connection = config('client.permission.database_connection');
         $this->table = $this->getConnection()->getDatabaseName().'.'.$this->getTable();
 
         parent::__construct($attributes);
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -8,7 +8,7 @@ class Role extends SpatieRole
 {
     public function __construct(array $attributes = [])
     {
-        $this->connection = config('client.database.connection');
+        $this->connection = config('client.permission.database_connection');
         $this->table = $this->getConnection()->getDatabaseName().'.'.$this->getTable();
 
         parent::__construct($attributes);

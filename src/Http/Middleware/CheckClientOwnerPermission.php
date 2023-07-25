324e8aa28bf8a26be93ffce8e3de0b60e6610536
Ata <quetzalskeith@gmail.com>
2023-07-25 17:50:24 +0700

    Added check client owner permission middleware

---
+++ src/Http/Middleware/CheckClientOwnerPermission.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace QuetzalStudio\OAuthClient\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Laravel\Passport\Exceptions\AuthenticationException;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckClientOwnerPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $permission): Response
+    {
+        if (! Request::hasMacro('client') || ! $request->client()->owner) {
+            throw new AuthenticationException();
+        }
+
+        $permissions = is_array($permission)
+            ? $permission
+            : explode('|', $permission);
+
+        foreach ($permissions as $permission) {
+            if ($request->client()->owner->hasPermissionTo($permission, config('client.permission.guard'))) {
+                return $next($request);
+            }
+        }
+
+        throw UnauthorizedException::forPermissions($permissions);
+    }
+}

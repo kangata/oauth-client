a5002dc1ffc60b2c33f595ed128f44e2c5ed5599
Ata <quetzalskeith@gmail.com>
2024-04-02 07:02:08 +0700

    Update config

--- config/client.php
+++ config/client.php
@@ -2,11 +2,13 @@
 
 return [
     'database' => [
-        'connection' => env('OAUTH_CLIENT_DB_CONNECTION', 'mysql_client'),
+        'connection' => env('CLIENT_DB_CONNECTION', 'mysql_client'),
     ],
 
     'permission' => [
-        'database_connection' => env('CLIENT_PERMISSION_DB_CONNECTION', 'mysql_client'),
-        'guard' => env('OAUTH_CLIENT_PERMISSION_GUARD', 'client_owner'),
+        'database' => ['
+            connection' => env('CLIENT_PERMISSION_DB_CONNECTION', 'mysql_client'),
+        ],
+        'guard' => env('CLIENT_PERMISSION_GUARD', 'client_owner'),
     ],
 ];
--- config/permission.php
+++ config/permission.php
@@ -1,5 +1,7 @@
 <?php
 
+$dbConnection = env('CLIENT_PERMISSION_DB_CONNECTION', 'mysql_client');
+
 return [
 
     'models' => [
@@ -36,7 +38,7 @@ return [
          * default value but you may easily change it to any table you like.
          */
 
-        'roles' => config('database.connections.mysql_client.database').'.roles',
+        'roles' => config("database.connections.{$dbConnection}.database").'.roles',
 
         /*
          * When using the "HasPermissions" trait from this package, we need to know which
@@ -44,7 +46,7 @@ return [
          * default value but you may easily change it to any table you like.
          */
 
-        'permissions' => config('database.connections.mysql_client.database').'.permissions',
+        'permissions' => config("database.connections.{$dbConnection}.database").'.permissions',
 
         /*
          * When using the "HasPermissions" trait from this package, we need to know which
@@ -52,7 +54,7 @@ return [
          * basic default value but you may easily change it to any table you like.
          */
 
-        'model_has_permissions' => config('database.connections.mysql_client.database').'.model_has_permissions',
+        'model_has_permissions' => config("database.connections.{$dbConnection}.database").'.model_has_permissions',
 
         /*
          * When using the "HasRoles" trait from this package, we need to know which
@@ -60,7 +62,7 @@ return [
          * basic default value but you may easily change it to any table you like.
          */
 
-        'model_has_roles' => config('database.connections.mysql_client.database').'.model_has_roles',
+        'model_has_roles' => config("database.connections.{$dbConnection}.database").'.model_has_roles',
 
         /*
          * When using the "HasRoles" trait from this package, we need to know which
